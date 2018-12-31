5b2eb812105067b241f719bcc8a523b6445150f7
RobertoFalconi <falkons@outlook.com>
2018-12-31 10:35:20 +0100

    Bugfixes

--- js/filtri.js
+++ js/filtri.js
@@ -14,12 +14,12 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
                     regioni[i].ve2t + regioni[i].vu3t + regioni[i].ve3t + regioni[i].vu4t +
                     regioni[i].ve4t + regioni[i].put + regioni[i].pet;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].vu1f + regioni[i].ve1f + regioni[i].vu2f +
                     regioni[i].ve2f + regioni[i].vu3f + regioni[i].ve3f + regioni[i].vu4f +
                     regioni[i].ve4f + regioni[i].puf + regioni[i].pef;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].vu1m + regioni[i].ve1m + regioni[i].vu2m +
                     regioni[i].ve2m + regioni[i].vu3m + regioni[i].ve3m + regioni[i].vu4m +
                     regioni[i].ve4m + regioni[i].pum + regioni[i].pem;
@@ -30,11 +30,11 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
                 incidenti = regioni[i].vu1t + regioni[i].vu2t + regioni[i].vu3t
                     + regioni[i].vu4t + regioni[i].put;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].vu1f + regioni[i].vu2f + regioni[i].vu3f
                     + regioni[i].vu4f + regioni[i].puf;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].vu1m + regioni[i].vu2m + regioni[i].vu3m
                     + regioni[i].vu4m + regioni[i].pum;
             }
@@ -44,11 +44,11 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
                 incidenti = regioni[i].ve1t + regioni[i].ve2t + regioni[i].ve3t
                     + regioni[i].ve4t + regioni[i].pet;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].ve1f + regioni[i].ve2f + regioni[i].ve3f
                     + regioni[i].ve4f + regioni[i].pef;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].ve1m + regioni[i].ve2m + regioni[i].ve3m
                     + regioni[i].ve4m + regioni[i].pem;
             }
@@ -61,12 +61,12 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
                     regioni[i].ve2t + regioni[i].vu3t + regioni[i].ve3t + regioni[i].vu4t +
                     regioni[i].ve4t;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].vu1f + regioni[i].ve1f + regioni[i].vu2f +
                     regioni[i].ve2f + regioni[i].vu3f + regioni[i].ve3f + regioni[i].vu4f +
                     regioni[i].ve4f;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].vu1m + regioni[i].ve1m + regioni[i].vu2m +
                     regioni[i].ve2m + regioni[i].vu3m + regioni[i].ve3m + regioni[i].vu4m +
                     regioni[i].ve4m;
@@ -77,11 +77,11 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
                 incidenti = regioni[i].vu1t + regioni[i].vu2t + regioni[i].vu3t
                     + regioni[i].vu4t;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].vu1f + regioni[i].vu2f + regioni[i].vu3f
                     + regioni[i].vu4f;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].vu1m + regioni[i].vu2m + regioni[i].vu3m
                     + regioni[i].vu4m;
             }
@@ -91,11 +91,11 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
                 incidenti = regioni[i].ve1t + regioni[i].ve2t + regioni[i].ve3t
                     + regioni[i].ve4t;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].ve1f + regioni[i].ve2f + regioni[i].ve3f
                     + regioni[i].ve4f;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].ve1m + regioni[i].ve2m + regioni[i].ve3m
                     + regioni[i].ve4m;
             }
@@ -106,10 +106,10 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
             if (filtro_gravita === "all") {
                 incidenti = regioni[i].put + regioni[i].pet;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].puf + regioni[i].pef;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].pum + regioni[i].pem;
             }
         }
@@ -117,10 +117,10 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
             if (filtro_gravita === "all") {
                 incidenti = regioni[i].put;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].puf;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].pum;
             }
         }
@@ -128,19 +128,19 @@ function incidentiPatentatiFiltro(filtro_tipologia, filtro_strada, filtro_gravit
             if (filtro_gravita === "all") {
                 incidenti = regioni[i].pet;
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 incidenti = regioni[i].pef;
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 incidenti = regioni[i].pem;
             }
         }
     }
-    return {patentati: patentati, incidenti: incidenti};
+    return { patentati: patentati, incidenti: incidenti };
 }
 
 /* Restituisce tutti i rapporti incidenti/patentati di tutte le regioni a seconda dei filtri utilizzato */
-function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravita){
+function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravita) {
     var i;
     var rapporti = [];
     if (filtro_tipologia === "all") {
@@ -150,23 +150,23 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     incidenti = regioni[i].vu1t + regioni[i].ve1t + regioni[i].vu2t +
                         regioni[i].ve2t + regioni[i].vu3t + regioni[i].ve3t + regioni[i].vu4t +
                         regioni[i].ve4t + regioni[i].put + regioni[i].pet;
-                    rapporti.push((incidenti/PatentiPopolazione[i].patenti));
-                }
-            }
-            else if (filtro_gravita === "injured") {
-                for (i in regioni) {
-                    incidenti = regioni[i].vu1f + regioni[i].ve1f + regioni[i].vu2f +
-                        regioni[i].ve2f + regioni[i].vu3f + regioni[i].ve3f + regioni[i].vu4f +
-                        regioni[i].ve4f + regioni[i].puf + regioni[i].pef;
-                    rapporti.push((incidenti/PatentiPopolazione[i].patenti));
+                    rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
             else if (filtro_gravita === "dead") {
                 for (i in regioni) {
+                    incidenti = regioni[i].vu1f + regioni[i].ve1f + regioni[i].vu2f +
+                        regioni[i].ve2f + regioni[i].vu3f + regioni[i].ve3f + regioni[i].vu4f +
+                        regioni[i].ve4f + regioni[i].puf + regioni[i].pef;
+                    rapporti.push((incidenti / PatentiPopolazione[i].patenti));
+                }
+            }
+            else if (filtro_gravita === "injured") {
+                for (i in regioni) {
                     incidenti = regioni[i].vu1m + regioni[i].ve1m + regioni[i].vu2m +
                         regioni[i].ve2m + regioni[i].vu3m + regioni[i].ve3m + regioni[i].vu4m +
                         regioni[i].ve4m + regioni[i].pum + regioni[i].pem;
-                    rapporti.push((incidenti/PatentiPopolazione[i].patenti));
+                    rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
         }
@@ -175,17 +175,17 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                 for (i in regioni) {
                     incidenti = regioni[i].vu1t + regioni[i].vu2t + regioni[i].vu3t
                         + regioni[i].vu4t + regioni[i].put;
-                    rapporti.push((incidenti/PatentiPopolazione[i].patenti));
+                    rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].vu1f + regioni[i].vu2f + regioni[i].vu3f
                         + regioni[i].vu4f + regioni[i].puf;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].vu1m + regioni[i].vu2m + regioni[i].vu3m
                         + regioni[i].vu4m + regioni[i].pum;
@@ -201,14 +201,14 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].ve1f + regioni[i].ve2f + regioni[i].ve3f
                         + regioni[i].ve4f + regioni[i].pef;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].ve1m + regioni[i].ve2m + regioni[i].ve3m
                         + regioni[i].ve4m + regioni[i].pem;
@@ -227,7 +227,7 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].vu1f + regioni[i].ve1f + regioni[i].vu2f +
                         regioni[i].ve2f + regioni[i].vu3f + regioni[i].ve3f + regioni[i].vu4f +
@@ -235,7 +235,7 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].vu1m + regioni[i].ve1m + regioni[i].vu2m +
                         regioni[i].ve2m + regioni[i].vu3m + regioni[i].ve3m + regioni[i].vu4m +
@@ -252,14 +252,14 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].vu1f + regioni[i].vu2f + regioni[i].vu3f
                         + regioni[i].vu4f;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].vu1m + regioni[i].vu2m + regioni[i].vu3m
                         + regioni[i].vu4m;
@@ -275,14 +275,14 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].ve1f + regioni[i].ve2f + regioni[i].ve3f
                         + regioni[i].ve4f;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].ve1m + regioni[i].ve2m + regioni[i].ve3m
                         + regioni[i].ve4m;
@@ -299,13 +299,13 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].puf + regioni[i].pef;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].pum + regioni[i].pem;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
@@ -319,13 +319,13 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].puf;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].pum;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
@@ -339,13 +339,13 @@ function rapportiRegioni(regioni, filtro_tipologia, filtro_strada, filtro_gravit
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "injured") {
+            else if (filtro_gravita === "dead") {
                 for (i in regioni) {
                     incidenti = regioni[i].pef;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
                 }
             }
-            else if (filtro_gravita === "dead") {
+            else if (filtro_gravita === "injured") {
                 for (i in regioni) {
                     incidenti = regioni[i].pem;
                     rapporti.push((incidenti / PatentiPopolazione[i].patenti));
